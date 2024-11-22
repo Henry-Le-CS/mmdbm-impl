0782015a3ff214dbd95b0bbd691ae19e9191f363
Henry Le <lehieu2077@gmail.com>
2024-11-23 03:15:22 +0700

    feat: integrate server with NER

---
+++ server/main.py
@@ -0,0 +1,27 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+from src.di.deps import Dependencies
+from src.db import Database
+from src.svc.ner import Detector
+
+from src.http import Server
+from src.api import new_api_registry
+
+load_dotenv()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    
+    server = new_api_registry(Server(
+        host=os.getenv("SERVER_HOST", "0.0.0.0"),
+        port=os.getenv("SERVER_PORT", 5001),
+        debug=os.getenv("SERVER_DEBUG_MODE", False),
+        deps=Dependencies(
+            detector=Detector(),
+            db=Database(os.getenv("DATABASE_URI"))
+        )
+    ))
+
+    server.run()
